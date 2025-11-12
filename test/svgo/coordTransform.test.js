d7e60331b39b23ef918be511440836fccb9c61e8
John Kenny <johnkenny54@yahoo.com>
2025-11-11 19:10:15 -0800

    fix; normalize before multiply

--- test/svgo/coordTransform.test.js
+++ test/svgo/coordTransform.test.js
@@ -1,17 +1,23 @@
 import { ExactNum } from '../../lib/exactnum.js';
 import { CoordTransform } from '../../lib/svgo/coordTransform.js';
 
-const tests = [{ transform: '', in: [0, 0], expected: [10, 260] }];
+const tests = [
+  {
+    transform: 'translate(10, 260) rotate(-90)',
+    in: [0, 0],
+    expected: [10, 260],
+  },
+];
 
 for (const test of tests) {
   it(test.transform, () => {
     const t = new CoordTransform(test.transform);
     const out = t.transform(new ExactNum(test.in[0]), new ExactNum(test.in[1]));
     if (out === null) {
-      expect(out).toBeNull();
+      expect(test.expected).toBeNull();
     } else {
-      expect(out[0]).toBe(test.expected[0]);
-      expect(out[1]).toBe(test.expected[1]);
+      expect(out[0].getValue()).toBe(test.expected[0]);
+      expect(out[1].getValue()).toBe(test.expected[1]);
     }
   });
 }
