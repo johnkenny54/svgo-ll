b9f7508f2040f0be066f8e48429039009b3fa165
John Kenny <johnkenny54@yahoo.com>
2025-11-11 19:13:06 -0800

    chore: test for null

--- test/svgo/coordTransform.test.js
+++ test/svgo/coordTransform.test.js
@@ -7,6 +7,12 @@ const tests = [
     in: [0, 0],
     expected: [10, 260],
   },
+  // No exact transform.
+  {
+    transform: 'translate(10, 260) rotate(-9)',
+    in: [0, 0],
+    expected: null,
+  },
 ];
 
 for (const test of tests) {
@@ -15,6 +21,8 @@ for (const test of tests) {
     const out = t.transform(new ExactNum(test.in[0]), new ExactNum(test.in[1]));
     if (out === null) {
       expect(test.expected).toBeNull();
+    } else if (test.expected === null) {
+      expect(out).toBeNull();
     } else {
       expect(out[0].getValue()).toBe(test.expected[0]);
       expect(out[1].getValue()).toBe(test.expected[1]);
